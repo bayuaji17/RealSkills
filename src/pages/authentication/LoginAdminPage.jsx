35e911ae49fa2bbc9a618c96eba9690d982e6a6f
Dwi Agus S <dwiagus435@gmail.com>
2023-11-22 20:36:44 +0700

    login admin

---
+++ src/pages/authentication/LoginAdminPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const LoginAdminPage = () => {
+  return <div>LoginAdminPage</div>;
+};
--- src/router/RouterPages.jsx
+++ src/router/RouterPages.jsx
@@ -4,6 +4,7 @@ import { Beranda } from "../pages/Beranda";
 import { LoginPage } from "../pages/authentication/LoginPage";
 import { RegisterPage } from "../pages/authentication/RegisterPage";
 import ResetPasswordPage from "../pages/authentication/ResetPasswordPage";
+import { LoginAdminPage } from "../pages/authentication/LoginAdminPage";
 
 export const RouterPages = () => {
   return (
@@ -11,6 +12,7 @@ export const RouterPages = () => {
       <Routes>
         <Route path="/" element={<Beranda />} />
         <Route path="login" element={<LoginPage />} />
+        <Route path="login admin" element={<LoginAdminPage />} />
         <Route path="register" element={<RegisterPage />} />
         <Route path="reset" element={<ResetPasswordPage />} />
       </Routes>
