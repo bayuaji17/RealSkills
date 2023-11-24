185052a38f8534726c8c4609420a2c50b9d31a7b
Dwi Agus S <dwiagus435@gmail.com>
2023-11-24 22:44:08 +0700

    update login admin

--- src/pages/authentication/LoginAdminPage.jsx
+++ src/pages/authentication/LoginAdminPage.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const LoginAdminPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const password = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="parent flex h-screen text-white">
       <div className=" sectionKiri flex w-[40%] h-full bg-[#6148FF] font-montserrat text-center text-2xl justify-center items-center">
@@ -8,25 +15,32 @@ export const LoginAdminPage = () => {
       </div>
       <div className=" sectionKanan flex w-[60%] h-full justify-center items-center bg-white">
         <div className=" flex flex-col justify-center items-center font-poppins">
-          <h1 className=" mb-4 font-bold text-3xl font-montserrat text-[#6148FF]">
+          <h1 className=" mb-3 font-bold text-3xl font-montserrat text-[#6148FF]">
             Login
           </h1>
           <div className="w-[23rem] h-[14rem] justify-center items-center font-poppins space-y-3 ">
-            <span className="text-sm text-[0.9rem] text-black">ID Admin</span>
+            <label className="text-sm text-[0.9rem] text-black">ID Admin</label>
             <input
               className="border border-black w-full py-3 px-2 rounded-2xl"
               type="text"
               placeholder="ID Admin"
             />
-            <div className="flex justify-between font-poppins text-[0.9rem]">
-              <span className="text-sm text-black">Password</span>
-              <span className="text-sm text-[#6148FF]">Lupa Kata Sandi</span>
-            </div>
+            <span className="text-sm text-black flex justify-between">
+              <label> Password</label>
+              <a href="/reset" className="text-[#6148FF]">
+                Lupa Kata Sandi{" "}
+              </a>
+            </span>
 
             <input
               className="border border-black w-full py-3 px-2 rounded-2xl"
-              type="password"
+              type={showPassword ? "text" : "password"}
               placeholder="Masukan Password"
+            />
+            <FontAwesomeIcon
+              icon={showPassword}
+              className="absolute top-2 right-2 cursor-pointer"
+              onClick={password}
             />
             <button className="bg-[#6148FF] w-full text-white rounded-2xl py-3 ">
               Masuk
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-       fontFamily: {
-        'montserrat': ['Montserrat', 'sans-serif'],
-         'poppins': ['Poppins', 'sans'],
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        poppins: ["Poppins", "sans"],
       },
       colors: {
-        'dark-blue' : '#6148FF'
+        "dark-blue": "#6148FF",
       },
     },
   },
