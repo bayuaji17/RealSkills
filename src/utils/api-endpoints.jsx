55e64228b9fe64060bac40e1facb0f993d63ccec
VinnTech <kevinginting605@gmail.com>
2023-11-29 16:40:53 +0700

    adjust file

--- src/utils/api-endpoints.jsx
+++
@@ -1,8 +0,0 @@
-export const API_ENDPOINTS = {
-    REGISTER_USER : "/api/auth/forgot-password",
-    LOGIN_USER : "/api/v1/auth/login",
-    GET_USER : "/api/v1/auth/me",
-    POPULAR : "/api/v1/movie/popular",
-    SEARCH_MOVIE : "/api/v1/search/movie",
-    DETAIL_MOVIE : "/api/v1/movie/"
-}
